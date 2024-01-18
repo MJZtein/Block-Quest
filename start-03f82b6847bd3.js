d5cd294ce7e6822bf17fe08ce0ed38ce56c08b41
Ztein <96412380+MJZtein@users.noreply.github.com>
2024-01-18 17:12:51 +0800

    Add files via upload

---
+++ start-03f82b6847bd3.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function() {
+
+    function Start() {
+        window.cr_createRuntime({
+            exportType: "html5"
+        });
+    };
+
+    if (document.readyState === "loading")
+        document.addEventListener("DOMContentLoaded", Start);
+    else
+        Start();
+
+})();
